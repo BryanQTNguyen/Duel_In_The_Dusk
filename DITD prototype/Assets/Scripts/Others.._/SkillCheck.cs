b43b64c8fa073d6b76f1fa1566339b2469e9adb4
Bryan Nguyen <bryan.308466@gmail.com>
2023-09-20 09:47:48 -0700

    More progress on the skill check bar

--- DITD prototype/Assets/Scripts/Others.._/SkillCheck.cs
+++ DITD prototype/Assets/Scripts/Others.._/SkillCheck.cs
@@ -6,27 +6,59 @@ using UnityEngine.UI;
 public class SkillCheck : MonoBehaviour
 {
     public Slider mSlider;
-    public bool slay = false;
+    public bool index; //this is used to determine which direction the marker faces
+    public float markerSpeed;
+    public bool playerShot;
+    public float playerAccuracy;
+    [SerializeField] private CanvasGroup canvasGroup; //will also work as a variable which keeps track of when its draw time
     // Start is called before the first frame update
     void Start()
     {
-        mSlider.value = 0;
+        canvasGroup.alpha = 0;
+        mSlider.value = 100;
+        playerShot = false;
+        index = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(mSlider.value < mSlider.maxValue && slay == false)
+        if (canvasGroup.alpha == 1)
         {
-            mSlider.value += 2;
-            slay = false;
+            if (Input.GetKeyDown("space")) //Player shoot
+            {
+                playerShot = true;
+            }
+
+            if (mSlider.value == mSlider.maxValue)
+            {
+                index = true;
+            }
+            if (mSlider.value == mSlider.minValue)
+            {
+                index = false;
+            }
         }
-        else
+    }
+    void FixedUpdate()
+    {
+        if(playerShot == false && canvasGroup.alpha == 1)
         {
-            mSlider.value -= 2;
-            slay = true;
-            
+            if (index == false)
+            {
+                mSlider.value += markerSpeed;
+            }
+            else if (index == true)
+            {
+                mSlider.value -= markerSpeed;
+            }
+        }
+        else if(playerShot == true)
+        {
+            playerAccuracy = mSlider.value;
+            Debug.Log("Player Shot with an accuracy of: " + playerAccuracy);
         }
 
     }
+
 }
