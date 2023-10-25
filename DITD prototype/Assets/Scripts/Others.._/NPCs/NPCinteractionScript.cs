5bd5d51b1c90ae65f59362b03c7c432e25a8c8a4
Bryan Nguyen <bryan.308466@gmail.com>
2023-10-25 09:56:22 -0700

    more fix

--- DITD prototype/Assets/Scripts/Others.._/NPCs/NPCinteractionScript.cs
+++ DITD prototype/Assets/Scripts/Others.._/NPCs/NPCinteractionScript.cs
@@ -13,6 +13,11 @@ public class NPCinteractionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         
     }
 }
