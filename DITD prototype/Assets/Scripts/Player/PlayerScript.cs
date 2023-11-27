d9011ae65a134900fd02064d9c26b350295ff66d
Bryan Nguyen <bryan.308466@gmail.com>
2023-11-27 09:52:09 -0800

    asdf

--- DITD prototype/Assets/Scripts/Player/PlayerScript.cs
+++ DITD prototype/Assets/Scripts/Player/PlayerScript.cs
@@ -6,66 +6,27 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    //Health variables
-    public int HealthCurrent;
-    public int HealthMax = 100;
-    public PlayerHealth healthBar;
+    //this scirpt will mainly communicate with the game manager
+    public float damageType;
+ /*
+ 1 = regular shot (33 damage)
+ 2 = instantKill (just reduce the health to 0 right away)
+ 3 = mini shot (reduce the health by 10)
 
-    public Rigidbody2D rb;
+ */
+    public bool bleed;
 
-    //Weapon variables 
-    public int Ammo;
-    public int gunType; // public variable made to inform other scripts that the player has this gun equipped
 
 
     // Start is called before the first frame update
     void Start()
     {
-        gunType = 0;
-        Ammo = 6; //character starts with revolver
-        HealthCurrent = HealthMax;
-        healthBar.SetMaxHealth(HealthMax);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (HealthCurrent <= 0)
-        {
-            Death();
-        }
-        /* health bar test
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Damage(10);
-        }
-        */
-    }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Shotgun")
-        {
-            Ammo = 2;
-            gunType = 1; 
-        }
-        if (collision.gameObject.tag == "Rifle")
-        {
-            Ammo = 12;
-            gunType = 2;
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "Enemy")
-            rb.velocity = Vector2.zero;
-    }
-    public void Damage(int amount) //damage function with a parameter for varying damages
-    {
-        HealthCurrent = HealthCurrent - amount;
-        healthBar.SetHealth(HealthCurrent);
-    }
-    public void Death()
-    {
-        //load ending screen
+
     }
 }
--- DITD prototype/Assets/gameManager.cs
+++ DITD prototype/Assets/gameManager.cs
@@ -7,15 +7,40 @@ public class gameManager : MonoBehaviour
 {
     public static gameManager Instance;
     public float enemyType;
-    [SerializeField] DialgoueManager dialogueManager;
-    [SerializeField] GameObject dialogueObject; 
     /*
-     1 = deputy enemy 
-     2 = ranger enemy 
-     3 = cactus enemy 
-     4 = banker boss
-     5 = sheriff boss
+    1 = deputy enemy 
+    2 = ranger enemy 
+    3 = cactus enemy 
+    4 = banker boss
+    5 = sheriff boss
+    */
+
+
+    [SerializeField] GameObject Character; //this is used to check the varibale damage is true
+    [SerializeField] PlayerScript playerScript;
+
+    //health stuff
+    public int HealthCurrent;
+    public int HealthMax = 100;
+    public PlayerHealth healthBar;
+
+    //damage stuff
+    public int regularShot = 33;
+    public int instantKill = 100;
+    public int bleedDamage = 2;
+    public int miniShot = 10;
+
+    public Rigidbody2D rb;
+
+    //Weapon variables 
+    public int Ammo;
+    public int gunType; // public variable made to inform other scripts that the player has this gun equipped
+    /*
+     0 = revolver 
+     1 = rifle
+     2 = shotgun
      */
+
 
     private void Awake()
     {
@@ -34,26 +59,59 @@ public class gameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gunType = 0;
+        Ammo = 6; //character starts with revolver
+        HealthCurrent = HealthMax;
+        healthBar.SetMaxHealth(HealthMax);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "cutSceneFirst" || SceneManager.GetActiveScene().name == "Train Station" || SceneManager.GetActiveScene().name
-            == "Bank Interrior")
+        if (HealthCurrent <= 0)
         {
-            dialogueObject = GameObject.FindWithTag("dialogueManager");
-            dialogueManager = dialogueObject.GetComponent<DialgoueManager>();
-            if (dialogueManager != null)
+            Death();
+        }
+        /* health bar test
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Damage(10);
+        }
+        */
+        if (SceneManager.GetActiveScene().name == "Train Station" || SceneManager.GetActiveScene().name
+            == "Bank Interrior") //checking if the scene is playable
+        {
+            Character = GameObject.FindWithTag("Player");
+            playerScript = Character.GetComponent<PlayerScript>();
+            if (playerScript != null)
             {
-
-
+                Damage();
+                Bleed();
             }
             else
             {
-                Debug.Log("Cannot find the Dialogue Manager (line 73)");
+                Debug.Log("Cannot locate the player");
             }
         }
+    }
+    public void Damage() //damage function with a parameter for varying damages
+    {
+        if (playerScript.damageType == 1)
+            HealthCurrent = HealthCurrent - regularShot;
+        if (playerScript.damageType == 2)
+            HealthCurrent = HealthCurrent - instantKill;
+        if (playerScript.damageType == 3)
+            HealthCurrent = HealthCurrent - miniShot;
+        
+    }
+    public void Bleed()
+    {
+        if (playerScript.bleed == true)
+            HealthCurrent = HealthCurrent - bleedDamage;
+    }
+
+    public void Death()
+    {
+        //load ending screen
     }
 }
